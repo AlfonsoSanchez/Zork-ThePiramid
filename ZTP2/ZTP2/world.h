acaa2e8afdfdc1352d800ebbb296c84511d3cbd8
AlfonsoSanchez <alfonsosanchez1896@gmail.com>
2016-03-16 18:23:52 +0100

    Re-start

--- ZTP2/ZTP2/world.h
+++ ZTP2/ZTP2/world.h
@@ -1,30 +1,3 @@
 #ifndef _WORLD_
 #define _WORLD_
 #include <stdio.h>
-#include <stdlib.h>
-#include "rooms.h"
-#include "exit.h"
-#include "player.h"
-class Player;
-class Exit;
-class Rooms;
-
-class World{
-public:
-	Rooms* room = nullptr;
-	Exit* exits = nullptr;
-	Player* player = nullptr;
-
-	World();
-	~World();
-
-	void CreateWorld()const;
-	void CreateExits();
-
-	
-
-
-
-
-};
-#endif
