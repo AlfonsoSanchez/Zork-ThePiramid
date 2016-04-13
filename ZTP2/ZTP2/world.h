ad14f0dc38ce026ce00a20b889bd5d4ff9e1bec9
AlfonsoSanchez <alfonsosanchez1896@gmail.com>
2016-04-13 17:49:36 +0200

    ADD vector.h

--- ZTP2/ZTP2/world.h
+++ ZTP2/ZTP2/world.h
@@ -1,12 +1,17 @@
 #ifndef _WORLD_
 #define _WORLD_
 
+#include "Entity.h"
+#include "exit.h"
+#include "item.h"
+#include"mString.h"
+#include"player.h"
+#include"rooms.h"
 
+#define NUM_ROOMS 12;
+#define NUM_EXITS 28;
+#define NUM_ITEMS 10;
 
-class Room;
-class Exit;
-class Player;
-class Item;
 
 class World{
 public:
